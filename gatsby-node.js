b0339921867dfba8ea3cc49bfe52ec946656323d
theseanco <theseancotterill@gmail.com>
2019-01-10 18:26:15 +0000

    root pages building

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,50 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+/*
 
-// You can delete this file if you're not using it
+Function taken from https://www.gatsbyjs.org/docs/creating-and-modifying-pages/
+
+*/
+
+var nodePath = require('path')
+// Implement the Gatsby API “createPages”. This is called once the
+// data layer is bootstrapped to let plugins create pages from data.
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return new Promise((resolve, reject) => {
+    const blogPostTemplate = nodePath.resolve(`src/pages/post-lorem.js`)
+    // Query for markdown nodes to use in creating pages.
+    resolve(
+      graphql(
+        `
+          {
+            allContentfulCategory {
+              edges {
+                node {
+                  slug
+                }
+              }
+            }
+          }
+        `
+      ).then(result => {
+        if (result.errors) {
+          reject(result.errors)
+        }
+
+        // Create pages for each markdown file.
+        result.data.allContentfulCategory.edges.forEach(({ node }) => {
+          const path =  `/${node.slug}`
+          createPage({
+            path,
+            component: blogPostTemplate,
+            // In your blog post template's graphql query, you can use path
+            // as a GraphQL variable to query for data from the markdown file.
+            context: {
+              path,
+            },
+          })
+        })
+      })
+    )
+  })
+}
