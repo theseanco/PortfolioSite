080035ae2ed33491587285119e32d00c3ece1ae8
theseanco <theseancotterill@gmail.com>
2019-01-19 12:08:27 +0000

    fixed most of the styling bug by actually reading the Gatsby docs. Intro animation still doesn't work

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,9 @@
  */
 
 // You can delete this file if you're not using it
+
+import "./src/components/header.css";
+import "./src/components/layout_new.css";
+import "./src/pages/authorStyling.css";
+import "./src/pages/categoryStyling.css";
+import "./src/pages/workStyling.css"
