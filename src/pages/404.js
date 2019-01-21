8a09c562dc5852bbe397c1c91626575974d66bcc
theseanco <theseancotterill@gmail.com>
2019-01-21 09:59:05 +0000

    styled 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
+import {Link} from 'gatsby'
+
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>404: NOT FOUND</h1>
+    <h3>Return <Link to="/" state={{}}>Home</Link></h3>
   </Layout>
 )
 
