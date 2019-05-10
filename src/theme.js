c3de5b53b5d07a72a69b3e2a504399fff5bfc5c1
theseanco <theseancotterill@live.com>
2019-05-10 22:55:37 +0100

    Added media queries and colours to theme

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,38 @@
+import { css } from 'styled-components'
+
 const colors = {
-  newColor: 'goldenrod'
+  stormy: '#494E6B',
+  cloud: '#98878F',
+  sunset: '#985E6D',
+  evening: '#192231',
+};
+
+const animations = {
+  fadein: '.3s',
+  revealAnimation: '3s'
 }
 
-const theme = {colors}
+const sizes = {
+  desktop: 992,
+  tablet: 768,
+  phone: 576,
+}
+
+// Iterate through the sizes and create a media template
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (min-width: ${sizes[label] / 16}em) {
+      ${css(...args)}
+    }
+  `
+  return acc
+}, {})
+
+const theme = {
+  colors, 
+  animations,
+	sizes,
+	media
+}
 
 export default theme
