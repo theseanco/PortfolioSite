5d07e21221f8f82f6269fcfe9201f013da8bdced
theseanco <theseancotterill@live.com>
2019-05-12 14:53:19 +0100

    some styling done, CSS reset needed

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,16 @@
+/*
+ *   todo:
+ *   - That wierd span that is in the wrong place next to the list
+ *   - Rest of the styled-components
+ *   - Is list of imgs accessible?
+ *   - ***CSS RESET!!!*** It's partially implemented in Layout but not working fully
+ */
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
 
+// Overall footer conatainer
 const Footer = styled.footer`
   align-items: center;
   border-top: 2px solid ${props => props.theme.colors.stormy};
@@ -14,14 +22,61 @@ const Footer = styled.footer`
   margin-top: 2rem;
   padding: 1em 0;
 
-	/* Uses media queries for desktop styling */
-	${props => props.theme.media.desktop`
+  /* Uses media queries for desktop styling */
+  ${props => props.theme.media.desktop`
 		align-items: center;
 		font-size: 1em;
 		height: 3rem;
 		justify-content: space-between;
 		padding: 1em 2em;
 	`}
+`
+
+// Display my name and date
+const FooterInfo = styled.div`
+  display: none;
+
+  ${props => props.theme.media.desktop`
+    color: White;
+    display: block;
+  `}
+`
+
+const FooterLeftContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin: 0;
+  padding: 0;
+`
+
+
+// Styling for list of project build icons
+const FooterIcons = styled.ul`
+  
+  display: flex;
+  list-style-type: none;
+  flex-direction: row;
+
+  li:not(:last-child) {
+    margin-right: 10px;
+  }
+`
+
+// The build icons themselves
+const FooterIcon = styled.img`
+  height: 15px;
+  margin-bottom: 0px;
+  width: 15px;
+
+  ${props => props.theme.media.tablet`
+    height: 25px;
+    width: 25px;
+  `}
+`
+
+const CreatedUsing = styled.span`
+  color: White
 `
 
 const PageFooter = ({ data }) => (
@@ -42,21 +97,24 @@ const PageFooter = ({ data }) => (
     render={data => (
       <>
         <Footer>
-          <div className="footer-info">Sean Cotterill, 2019</div>
-          <ul className="footer-icons">
-              <span style={{ marginRight: `10px`, color: `White` }}>
+          <FooterInfo>Sean Cotterill, 2019</FooterInfo>
+          <FooterLeftContainer>
+            <CreatedUsing>
               Created using:
-            </span>
-            {data.builtIcons.technologyIcons.map(data => {
-              return (
-                <li key={data.id} className="footer-icon">
-                  <div className="footer-icon" key={data.id}>
-                    <img src={`https://${data.file.url}`} alt={data.title} />
-                  </div>
-                </li>
-              )
-            })}
-          </ul>
+            </CreatedUsing>
+            <FooterIcons>
+              {data.builtIcons.technologyIcons.map(data => {
+                return (
+                  <li key={data.id}>
+                    <FooterIcon
+                      src={`https://${data.file.url}`}
+                      alt={data.title}
+                    />
+                  </li>
+                )
+              })}
+            </FooterIcons>
+          </FooterLeftContainer>
         </Footer>
       </>
     )}
--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,7 @@ The query here gets information about the title of the site, as well as icons to
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
-import styled, { ThemeProvider } from 'styled-components'
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components'
 // import './layout.css'
 
 import Header from './header'
@@ -15,6 +15,57 @@ import Footer from './footer'
 
 //styled-components theme
 import theme from '../theme'
+
+// CSS Reset
+const GlobalStyle = createGlobalStyle`
+	dy, div, span, applet, object, iframe,
+	h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+	a, abbr, acronym, address, big, cite, code,
+	del, dfn, em, img, ins, kbd, q, s, samp,
+	small, strike, strong, sub, sup, tt, var,
+	b, u, i, center,
+	dl, dt, dd, ol, ul, li,
+	fieldset, form, label, legend,
+	table, caption, tbody, tfoot, thead, tr, th, td,
+	article, aside, canvas, details, embed,
+	figure, figcaption, footer, header, hgroup,
+	main, menu, nav, output, ruby, section, summary,
+	time, mark, audio, video {
+		margin: 0;
+		padding: 0;
+		border: 0;
+		font-size: 100%;
+		font: inherit;
+		vertical-align: baseline;
+	}
+	/* HTML5 display-role reset for older browsers */
+	article, aside, details, figcaption, figure,
+	footer, header, hgroup, main, menu, nav, section {
+		display: block;
+	}
+	/* HTML5 hidden-attribute fix for newer browsers */
+	*[hidden] {
+			display: none;
+	}
+	body {
+		line-height: 1;
+	}
+	ol, ul {
+		list-style: none;
+	}
+	blockquote, q {
+		quotes: none;
+	}
+	blockquote:before, blockquote:after,
+	q:before, q:after {
+		content: '';
+		content: none;
+	}
+	table {
+		border-collapse: collapse;
+		border-spacing: 0;
+	}
+`
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -38,13 +89,13 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <ThemeProvider theme={theme}>
-          <div className="master-container">
-            <div className="body-container">
-              <Header siteTitle={data.homepageInfo.homepageTitle} />
-              {children}
-              <Footer />
-            </div>
-          </div>
+						<div className="master-container">
+							<div className="body-container">
+								<Header siteTitle={data.homepageInfo.homepageTitle} />
+								{children}
+								<Footer />
+							</div>
+						</div>
         </ThemeProvider>
       </>
     )}
