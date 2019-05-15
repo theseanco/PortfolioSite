ecd8dae5b21defacabc8b1a50e36d7d77fc20203
theseanco <theseancotterill@gmail.com>
2019-05-15 22:23:04 +0100

    Some header CSS ported

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -20,17 +20,54 @@ const StyledHeader = styled.header`
   padding: 1em 2em;
 
   h1 {
-    display: none;
+    a {
+      display: none;
+      color: White;
+      margin: 0;
+    }
   }
 
   ${props => props.theme.media.tablet`
     h1 {
-      color: White;
-      display: block;
-      font-size: 2.5rem;
-      font-weight: 100;
-      letter-spacing: -4px;
-      text-decoration: none;
+
+      a {
+        color: White;
+        display: block;
+        font-size: 2.5rem;
+        font-weight: 100;
+        letter-spacing: -4px;
+        text-decoration: none;
+      }
+
+    }
+  `}
+`
+
+const HeaderLinkList = styled.nav`
+  list-style-type: none;
+  display: flex;
+  flex-direction: row;
+  margin: 0;
+  justify-content: space-between;
+
+  a {
+    font-size: 1.25rem;
+    padding: 0 1rem;
+    margin: 0;
+    color: White;
+  }
+
+  ${props => props.theme.media.tablet`
+    padding: 0 0.4rem
+
+    a {
+      width: 100%
+    }
+  `}
+
+  ${props => props.theme.media.desktop`
+    a {
+      padding: 0 1rem;
     }
   `}
 `
@@ -52,24 +89,19 @@ const Header = ({ siteTitle }) => (
     `}
     render={data => (
       <StyledHeader>
-        <h1 className="header-big-home-link">
+        <h1>
           {/*THESE ARE CONDITIONALLY RENDERED BASED ON MEDIA QUERIES!*/}
           <Link
             to="/"
             state={{
               noAnimation: true,
             }}
-            style={{
-              color: `inherit`,
-              textDecoration: `none`,
-            }}
           >
             {siteTitle}
           </Link>
         </h1>
 
-        <ul className="header-link-list">
-          <li>
+        <HeaderLinkList>
             <Link
               className="header-small-home-link"
               to="/"
@@ -79,7 +111,6 @@ const Header = ({ siteTitle }) => (
             >
               Home
             </Link>
-          </li>
           {data.contentfulAuthor.linkList.links.map(data => {
             return (
               <li key={`header-${data.LinkType}`}>
@@ -89,12 +120,10 @@ const Header = ({ siteTitle }) => (
               </li>
             )
           })}
-          <li>
             <Link className="header-link" to="/about">
               About
             </Link>
-          </li>
-        </ul>
+        </HeaderLinkList>
       </StyledHeader>
     )}
   />
